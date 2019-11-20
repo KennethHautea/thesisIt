ca1aa395ba71048445992c778d881b5a4d820b77
KennethHautea <lichking20011@gmail.com>
2019-11-20 22:00:17 +0800

    merge php files

---
+++ New folder/navbar.php
@@ -0,0 +1,23 @@
+<div class="logo">
+	<img x="0" y="0" width="100%" height="100%" src= "logo.png"></img>
+</div>
+<div id="navbar">
+	<div id="Create_Event">
+		<a href="#">Create Event</a>
+	</div>
+	<div id="Events">
+		<a href="#">Events</a>
+	</div>
+	<div id="Account">
+		<a href="#">Account</a>
+	</div>
+	<div id="Help_Center">
+		<a href="#">Help Center</a>
+	</div>
+	<div id="Login">
+		<a href="login.html">Login</a>
+	</div>
+</div>
+<svg class="Line_1">
+	<path fill="transparent" stroke="rgba(112,112,112,1)" stroke-width="1px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Line_1" d="M 0 0 L 1280 0"></path>
+</svg>
