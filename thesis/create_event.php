a02602b6ad1091fd7028d935b8065fa05ef606c7
unknown <ermitanyongbungol@gmail.com>
2019-11-24 20:34:44 +0800

    update

--- thesis/create_event.php
+++ thesis/create_event.php
@@ -15,22 +15,56 @@
 <form method="POST" onsubmit="return validate()">
 <div id="Create_Eventform">
 <?php include 'navbar.php' ?>
+
+<?php include 'config.php';
+
+	
+	if(isset($_POST['btn_saveevent'])) {
+		if(!empty($_POST['eventname']) || !empty($_POST['eventenddate'])){
+			$eventname = $_POST['eventname'];
+			$eventenddate = $_POST['eventenddate'];
+			$sql_n = "SELECT * FROM eventdetails WHERE EventName = '$eventname'";
+			$res_n = mysqli_query($conn, $sql_n);
+		if(mysqli_num_rows($res_n) > 0){
+			echo '<script type="text/javascript">  
+						if(window.alert("Event Name already existed! Please provide another name.")){
+							window.location.href = "index.php"
+						}
+					</script>';
+		}
+		else {
+			$result = mysqli_query($conn, "INSERT INTO eventdetails(EventName, EventEnd, EventStart) VALUES('$eventname', '$eventenddate', curdate())");
+			echo '<script type="text/javascript">  
+						if(window.alert("You have successfully created an event!")){
+							window.location.href="index.php"
+						}
+					</script>';
+		}
+		}
+	}
+	
+	?>
+	
 	<div id="save_event">
-		<input type="submit" name="" class="Rectangle_5" value="save event">
+		<input type="submit" name="btn_saveevent" class="Rectangle_5" value="save event">
 	</div>
+
 	<div id="event_name">
 		<div id="Event_Name">
 			<span>Event Name</span>
 		</div>
-		<input type="text" name="" class="edittext_EditText" required>
+		<input type="text" name="eventname" class="edittext_EditText" required>
 	</div>
+
 	<div id="end_time">
 		<div id="End_Date">
 			<span>End Date</span>
 		</div>
-		<input type="date" name="" class="datetime_EndTime" required>
-		
+		<input type="date" name="eventenddate" class="datetime_EndTime" required>
 	</div>
+
+	
+
 </div>
 </form>
 </body>
