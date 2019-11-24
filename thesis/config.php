f2f1fa6075d88a864dfe2d130e70ff908d4f2173
KennethHautea <lichking20011@gmail.com>
2019-11-24 17:32:29 +0800

    add session

--- thesis/config.php
+++ thesis/config.php
@@ -10,4 +10,19 @@ $conn  = mysqli_connect ($servername,$username,$password,$db_name);
 if(! $conn ) {
     die("Could not connect: " . mysql_error());
  }
+
+session_start();
+	$expireAfter = 20;
+
+if(isset($_SESSION['last_action'])){
+    $secondsInactive = time() - $_SESSION['last_action'];
+    $expireAfterSeconds = $expireAfter * 60;
+
+    if($secondsInactive >= $expireAfterSeconds){
+        session_unset();
+        session_destroy();
+    }
+    
+}
+$_SESSION['last_action'] = time(); 
 ?>
